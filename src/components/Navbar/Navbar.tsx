b3fc03d749ac37c3266e7fb95fee5d40803044f3
biprookas-wq <biproo.kas@gmail.com>
2026-09-14 16:28:10 +0600

    refined my code with AI

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,23 +2,28 @@ import Logo from "../../assets/logo-text.png";
 
 const Navbar = () => {
   return (
-    <nav className=" sticky top-0 z-50 border-b border-gray-200 bg-white">
-      <div className="flex justify-between items-center container mx-auto p-4">
-              <img src={Logo} alt="Logo" />
+    <nav className="sticky top-0 z-50 border-b border-gray-200 bg-white">
+      <div className="container mx-auto flex items-center justify-between p-4">
+        {/* Logo */}
+        <img src={Logo} alt="Logo" />
 
-      <ul className="flex space-x-4">
-        <li>Home</li>
-        <li>Technologies</li>
-        <li>Projects</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-      <div>
-        <button className="btn btn-ghost rounded-3xl">Sign In</button>
-        <button className="btn btn-active btn-secondary rounded-3xl">
-          Sign Up
-        </button>
-      </div>
+        {/* Navigation */}
+        <ul className="hidden space-x-4 md:flex">
+          <li>Home</li>
+          <li>Technologies</li>
+          <li>Projects</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+
+        {/* Buttons */}
+        <div>
+          <button className="btn btn-ghost rounded-3xl">Sign In</button>
+
+          <button className="btn btn-active btn-secondary rounded-3xl">
+            Sign Up
+          </button>
+        </div>
       </div>
     </nav>
   );
